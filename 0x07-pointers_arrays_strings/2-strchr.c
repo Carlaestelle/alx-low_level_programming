0d1dad3a01e7d53942a1659f7df81f39b1055089
Carlaestelle <carlamwasumbi25@gmail.com>
2023-01-04 07:12:42 -0800

    yessir

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,14 @@
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
-	{	
+	{
 		if (*s == c)
 			return (s);
 		s++;
-	}								}
+	}
 	/**
 	 * if c is '\0', you should return the pointer		 
-	 * 
+	 *
 	 * the pointer to the '\0' of the string s
 	 */
 	if (*s == c)
