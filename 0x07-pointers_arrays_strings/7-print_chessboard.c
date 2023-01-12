180a03bc4a0bae54d39d34a2b545914dae9a1fdc
Carlaestelle <carlamwasumbi25@gmail.com>
2023-01-12 11:57:51 -0800

    calm down

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,13 +2,16 @@
 
 /**
  * print_chessboard - a function that prints the chessboard
+ *
  * @a: 2d aray of chars
+ *
  * Return: nothing
  */
 
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	i = 0;
 	j = 0;
 	while (i < 64)
