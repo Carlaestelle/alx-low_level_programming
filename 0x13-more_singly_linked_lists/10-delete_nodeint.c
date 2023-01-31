4ca197e7b6c015af6d76c6e5de1acbf799c78f25
Carlaestelle <carlamwasumbi25@gmail.com>
2023-01-31 11:05:30 -0800

    git thius

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,6 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		next = (*head)->next;
@@ -24,15 +25,19 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = next;
 		return (1);
 	}
+
 	temp = *head;
+
 	for (i = 0; i < index - 1; i++)
 	{
 		if (temp->next == NULL)
 			return (-1);
 		temp = temp->next;
 	}
+
 	next = temp->next;
 	temp->next = next->next;
 	free(next);
+
 	return (1);
 }
