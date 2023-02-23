457e40fcb210e80a116e908d915a77f77ce99c4b
Carlaestelle <carlamwasumbi25@gmail.com>
2023-02-23 06:30:54 -0800

    hate that i love you

--- 0x08-recursion/0-puts_recursions.c
+++ 0x08-recursion/0-puts_recursions.c
@@ -1,17 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
+ * _puts_recursion - a function that prints a new line
  *
- * @s: string
+ * @s: the string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+ * Return: nothing
+*/
 
 void _puts_recursion(char *s)
 {
-	if (*s != '0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
