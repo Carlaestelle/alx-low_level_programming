17273811bdb8188fa8498e2e02096e311fdece4b
Carlaestelle <carlamwasumbi25@gmail.com>
2023-03-25 10:28:48 -0700

    Numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers of base16 in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
